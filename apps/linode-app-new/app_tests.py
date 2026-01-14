b9891a858a601a9b7ef9f2e1e2b2f0275bf2c4f8
ssastrau <ssastrau@akamai.com>
2026-01-14 12:22:27 +0100

    tests and deletion

---
+++ apps/linode-app-new/app_tests.py
@@ -0,0 +1,18 @@
+import sys
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+url = f"https://{sys.argv[1]}ip.linodeusercontent.com/"
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver.get(url)
+title = driver.title.lower()
+driver.quit()
+if "Jenkins" not in title:
+    print("Jenkins is not started")
+    sys.exit(1)
+print("Jenkins is started")
