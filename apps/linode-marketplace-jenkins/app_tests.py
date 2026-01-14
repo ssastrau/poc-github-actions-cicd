791399adbccf9be91fe149e626fdf52078f263ff
ssastrau <ssastrau@akamai.com>
2026-01-14 14:04:27 +0100

    final

--- apps/linode-marketplace-jenkins/app_tests.py
+++ apps/linode-marketplace-jenkins/app_tests.py
@@ -1,6 +1,7 @@
 import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 url = f"https://{sys.argv[1]}ip.linodeusercontent.com/"
@@ -8,10 +9,13 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), options=options)
-driver.get(url)
-title = driver.title.lower()
-driver.quit()
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+try:
+    driver.get(url)
+    title = driver.title.lower()
+finally:
+    driver.quit()
 if "Jenkins" not in title:
     print("Jenkins is not started")
     sys.exit(1)
