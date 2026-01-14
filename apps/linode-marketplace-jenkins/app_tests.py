e754597730af60fd93e3de5f73c4d6f6ed1e9c68
ssastrau <ssastrau@akamai.com>
2026-01-14 14:16:45 +0100

    final

--- apps/linode-marketplace-jenkins/app_tests.py
+++ apps/linode-marketplace-jenkins/app_tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-url = f"https://{sys.argv[1]}ip.linodeusercontent.com/"
+url = f"https://{sys.argv[1]}.ip.linodeusercontent.com/"
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
